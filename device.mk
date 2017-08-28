4d09596dd230ad7f6de5dc7bd278391692fe1bdd
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-28 18:27:01 -0500

    clark: Add HIDL HALs
    Change-Id: I06ace466002f8925054dee6069b212885194d850

--- device.mk
+++ device.mk
@@ -243,3 +243,28 @@ PRODUCT_PACKAGES += \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.light@2.0-impl \
+    android.hardware.renderscript@1.0-impl \
+    android.hardware.drm@1.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.power@1.0-impl \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.usb@1.0-service
+
+PRODUCT_COPY_FILES += \
+    device/motorola/clark/configs/manifest.xml:system/vendor/manifest.xml
