ef0a74a06fde437ca7497a073eb73352ffdc71d4
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    clark: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- lineage_clark.mk
+++ lineage_clark.mk
@@ -2,6 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
 # Inherit some common LineageOS stuff.
