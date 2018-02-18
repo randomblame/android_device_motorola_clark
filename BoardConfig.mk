c94774aebbefe7a6565132c7f55cc15fb1447367
Hashbang173 <hashbang173@gmail.com>
2018-02-18 16:01:06 -0500

    clark: include common qcom sepolicy
    Change-Id: I71889a7c9defd6f632a9e93ffa24d48232cf5b9b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,7 +153,8 @@ TARGET_RIL_VARIANT := caf
 TARGET_USE_SDCLANG := true
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 #BOARD_SEPOLICY_DIRS += \
 #    $(DEVICE_PATH)/sepolicy
