054119b2e72b2903752c6e5ac9e7f92b95ab839a
chrmhoffmann <chrmhoffmann@gmail.com>
2018-05-11 11:28:23 +0200

    clark: move ipacm to /vendor
    Change-Id: I68f86543b502424b2a2e96a479f65185006a6c70

--- data-ipa-cfg-mgr/ipanat/src/Android.mk
+++ data-ipa-cfg-mgr/ipanat/src/Android.mk
@@ -13,6 +13,8 @@ LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 LOCAL_SRC_FILES := ipa_nat_drv.c \
                    ipa_nat_drvi.c
 
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
 ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS := -DDEBUG
 endif
