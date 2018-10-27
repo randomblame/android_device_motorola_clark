fc555c12d2c118e0cf3f8aa15a8b869745504b6a
kelsiraed <dumbandroid@gmail.com>
2018-10-27 01:13:09 -0700

    move to basic usb service

--- device.mk
+++ device.mk
@@ -284,4 +284,4 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.thermal@1.0-impl \
     android.hardware.gnss@1.0-impl \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
