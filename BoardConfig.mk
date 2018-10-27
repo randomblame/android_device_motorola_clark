4d06b661f3f52d20db856367d8368626a94555b1
kelsiraed <dumbandroid@gmail.com>
2018-10-26 23:07:43 -0700

    exclusively build device/qcom/sepolicy-legacy/sepolicy.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_USE_SDCLANG := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 include device/qcom/sepolicy-legacy/sepolicy.mk
 
 #BOARD_SEPOLICY_DIRS += \
