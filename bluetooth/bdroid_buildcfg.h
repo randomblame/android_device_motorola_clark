810c190a6ea55fd67c494aec2505c4a9fda4db2c
kelsiraed <dumbandroid@gmail.com>
2018-12-12 15:22:50 -0800

    bluetooth

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,9 +18,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Moto X Pure"
+#define BTM_DEF_LOCAL_NAME "Moto X Pure"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
-#define BLUETOOTH_QTI_SW TRUE
-#define BLE_VND_INCLUDED   TRUE
+#define BLUETOOTH_QTI_SW          TRUE
+#define BLE_VND_INCLUDED          TRUE
 
 #endif
