e5ae389afbbf5f7f2195d771e026805c40c99f9e
Hashbang173 <hashbang173@gmail.com>
2019-01-22 18:15:22 -0500

    clark: remove pp_calib_data
    Change-Id: Ifaaecc8c68d5323c00cc67f50ea769120447a318

--- device.mk
+++ device.mk
@@ -81,9 +81,6 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_PACKAGES += \
-    pp_calib_data_mipi_mot_cmd_inx_QHD_0_570_v0.xml
-
-PRODUCT_PACKAGES += \
     hwcomposer.msm8992 \
     gralloc.msm8992 \
     copybit.msm8992 \
