89f32741da63de566257ce00ac209fc472ffb5db
kelsi <dumbandroid@gmail.com>
2020-01-09 21:16:31 -0800

    Clark: nfc - ISO C++17 does not allow 'register' storage c lass specifier [-Wregister]

--- nfc/src/adaptation/CrcChecksum.cpp
+++ nfc/src/adaptation/CrcChecksum.cpp
@@ -69,9 +69,9 @@ static const unsigned short crctab[256] = {
 **
 *******************************************************************************/
 unsigned short crcChecksumCompute(const unsigned char* buffer, int bufferLen) {
-  register unsigned short crc = 0;
-  const register unsigned char* cp = buffer;
-  register int cnt = bufferLen;
+  unsigned short crc = 0;
+  const unsigned char* cp = buffer;
+  int cnt = bufferLen;
 
   while (cnt--) {
     crc = ((crc >> 8) & 0xff) ^ crctab[(crc & 0xff) ^ *cp++];
