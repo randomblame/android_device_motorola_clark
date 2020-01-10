52131a3fc7ec564f96bfea72c15a03d0263a9420
kelsi <dumbandroid@gmail.com>
2020-01-10 13:58:58 -0800

    Revert "Revert "Clark: set USE_XML_AUDIO_POLICY_CONF := 1 (this needs to be fixed properly)""
    This reverts commit e24c059ac7706e91f5ef99f9f0ab4189647f364a.

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/motorola/clark/clark-vendor.mk)
 
 # Audio
+USE_XML_AUDIO_POLICY_CONF := 1
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
