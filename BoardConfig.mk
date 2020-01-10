6eb5d76c9d4de97d43f417adc8b87fcce850a50a
kelsi <dumbandroid@gmail.com>
2020-01-09 22:02:27 -0800

    Clark: temp noop sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,9 +172,6 @@ TARGET_RIL_VARIANT := caf
 #include device/qcom/sepolicy/sepolicy.mk
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy
-
 # Sensors
 BOARD_USES_MOT_SENSOR_HUB := true
 
