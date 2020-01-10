c9527061e802b3dedae60a89b2242feb25e7bb73
kelsi <dumbandroid@gmail.com>
2020-01-09 21:02:37 -0800

    Enable clearkey drm plugin v1.2

--- device.mk
+++ device.mk
@@ -280,7 +280,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
